4eae0a1b63a7e9ad8dbbce681ab80a4643b06798
Ashish <asaschoudhary616@gmail.com>
2020-02-26 14:50:02 +0530

    MODIFIED:Take three input

--- arthmeticSorting.sh
+++ arthmeticSorting.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -x
 #print the massage
 echo "Welcome the Arithmetic Operation"
+#!/bin/bash -x
+#Display the three input
+echo "Enter the first input"
+read a
+echo "Enter the second input"
+read b
+echo "Enter the third input"
+read c
+
+
 
 

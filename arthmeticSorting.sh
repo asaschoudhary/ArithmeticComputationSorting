c37ee3020e8233f6a0e2f81c149a3ce7ef556142
Ashish <asaschoudhary616@gmail.com>
2020-02-26 16:17:34 +0530

    MODIFIED: fourth aruthmetic operation

--- arthmeticSorting.sh
+++ arthmeticSorting.sh
@@ -14,6 +14,9 @@ compute1=`echo "scale=2 ; $a*$b+$c" | bc -l`
 echo $compute1
 compute2=`echo "scale=2 ; $c+$a/$b" | bc -l`
 echo $compute2
+compute3=1`echo "scale=2 ; $a%$b+$c" | bc -l`
+echo $compute3
+
 
 
 

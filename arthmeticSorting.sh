30700e392b5d49afa08f2974c316995315f45e8f
Ashish <asaschoudhary616@gmail.com>
2020-02-26 17:20:52 +0530

    MODIFIED: Store the value in Dictionary

--- arthmeticSorting.sh
+++ arthmeticSorting.sh
@@ -8,14 +8,31 @@ echo "Enter the second input"
 read b
 echo "Enter the third input"
 read c
+#compute=`echo "scale=2 ; $a+$b*$c" | bc -l`
+#echo $compute
+#compute1=`echo "scale=2 ; $a*$b+$c" | bc -l`
+#echo $compute1
+#compute2=`echo "scale=2 ; $c+$a/$b" | bc -l`
+#echo $compute2
+#compute3=1`echo "scale=2 ; $a%$b+$c" | bc -l`
+#echo $compute3
+declare -A arithmeticDictionary
 compute=`echo "scale=2 ; $a+$b*$c" | bc -l`
-echo $compute
+arithmeticDictionary[1]="$compute"
 compute1=`echo "scale=2 ; $a*$b+$c" | bc -l`
-echo $compute1
-compute2=`echo "scale=2 ; $c+$a/$b" | bc -l`
-echo $compute2
-compute3=1`echo "scale=2 ; $a%$b+$c" | bc -l`
-echo $compute3
+arithmeticDictionary[2]="$compute1"
+compute2=`echo "scale=2 ; $a%$b+$c" | bc -l`
+arithmeticDictionary[3]="$compute2"
+compute3=`echo "scale=2 ; $a%$b+$c" | bc -l`
+arithmeticDictionary[4]="$compute3"
+
+ 
+
+
+
+
+
+
 
 
 
